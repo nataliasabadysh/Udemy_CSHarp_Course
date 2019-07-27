c2abe34b6ab22e9573e6a93e124f97f38829af98
nataliasabadysh <nataliasabadysh@gmail.com>
2019-07-27 12:51:14 -0700

    list, array

--- FirstCourse/FirstCourse/Value&RefTypes/Example.cs
+++ FirstCourse/FirstCourse/Value&RefTypes/Example.cs
@@ -13,9 +13,7 @@ namespace FirstCourse.ValueRefTypes
 
             Console.WriteLine(string.Format("a: {0}, b: {1}", a, b)); // a= 10, b = 11
 
-
             // Reference Type
-
             var array1 = new int[3] { 1, 2, 3 };
             var array2 = array1;
             array2[0] = 0;
@@ -29,4 +27,3 @@ namespace FirstCourse.ValueRefTypes
         }
     }
 }
-
